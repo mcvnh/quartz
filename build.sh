b1bf4a8179ece161bc892fcfb070ccd09e3a1145
Mac <anh.mac@zozo.com>
2025-11-19 11:41:49 +0700

    add build file

--- build.sh
+++ build.sh
@@ -6,17 +6,7 @@ echo "$NOTES_SSH_KEY" > ~/.ssh/id_ed25519
 cat ~/.ssh/id_ed25519
 chmod 600 ~/.ssh/id_ed25519
 
-# Create SSH config file
-cat > ~/.ssh/config <<EOL
-Host github.com
-    Hostname github.com
-    IdentityFile=~/.ssh/id_ed25519
-    StrictHostKeyChecking no
-EOL
-chmod 600 ~/.ssh/config
-cat ~/.ssh/config
-
-git clone git@github.com:mcvnh/zetta.git notes
+GIT_SSH_COMMAND="ssh -i ~/.ssh/id_ed25519 -o StrictHostKeyChecking=no" git clone git@github.com:mcvnh/zetta.git notes
 
 # Replace Quartz content/ folder
 rm -rf content
