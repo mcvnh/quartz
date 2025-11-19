dec119889d456a7f3dedaea40d9109bcb9afed17
Mac <anh.mac@zozo.com>
2025-11-19 11:31:34 +0700

    add build file

--- build.sh
+++ build.sh
@@ -4,13 +4,12 @@ set -e
 mkdir -p ~/.ssh
 echo "$NOTES_SSH_KEY" > ~/.ssh/id_ed25519
 cat ~/.ssh/id_ed25519
-
 chmod 600 ~/.ssh/id_ed25519
-
-# Avoid "host verification" failure
 ssh-keyscan github.com >> ~/.ssh/known_hosts
-ssh -T git@github.com
-# Clone your private note repo
+
+# Debug: check key is loaded
+ls -la ~/.ssh
+
 git clone git@github.com:mcvnh/zetta.git notes
 
 # Replace Quartz content/ folder
