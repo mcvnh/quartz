aa5ef522ff5acd9086766823828e6a144302fdea
Mac <anh.mac@zozo.com>
2025-11-19 11:16:42 +0700

    add build file

---
+++ build.sh
@@ -0,0 +1,17 @@
+mkdir -p ~/.ssh
+echo "$NOTES_SSH_KEY" > ~/.ssh/id_ed25519
+chmod 600 ~/.ssh/id_ed25519
+
+# Avoid "host verification" failure
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+# Clone your private note repo
+git clone git@github.com:mcvnh/notes.git notes
+
+# Replace Quartz content/ folder
+rm -rf content
+cp -R notes/content ./content
+
+# Now build Quartz
+npm install
+npx quartz build
